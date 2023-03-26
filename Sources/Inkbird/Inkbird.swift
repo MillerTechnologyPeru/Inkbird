b9373e8ef036ffb8c823ec59affeb031d7a8bcd6
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-25 17:13:00 -0700

    Add `InkbirdAdvertisement.init(advertisementData:)`

--- Sources/Inkbird/Inkbird.swift
+++ Sources/Inkbird/Inkbird.swift
@@ -6,3 +6,14 @@ public enum InkbirdAdvertisement: Equatable, Hashable {
     
     case thermometer(Thermometer)
 }
+
+public extension InkbirdAdvertisement {
+    
+    init?<T: GATT.AdvertisementData>(_ advertisementData: T) {
+        if let thermometer = Thermometer(advertisementData) {
+            self = .thermometer(thermometer)
+        } else {
+            return nil
+        }
+    }
+}
